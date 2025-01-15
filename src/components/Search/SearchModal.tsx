76491918dc4f1c11ae128cfe62e66b9d9abafbcf
AmirHossein Jahankheir Amlashi <amir9877h@gmail.com>
2025-01-15 13:39:34 +0330

    fix Search Modal issue

--- src/components/Search/SearchModal.tsx
+++ src/components/Search/SearchModal.tsx
@@ -5,12 +5,15 @@ const SearchModal = ({ toggleSearchModal, isSearchOpen }) => {
   return (
     <>
       {isSearchOpen && (
-        <div
-          onClick={toggleSearchModal}
-          className={`flex justify-center pt-16 w-full h-full fixed top-0 left-0 bg-gray-900 bg-opacity-80`}
-        >
-          <Search className={"h-[80px]"} />
-        </div>
+        <>
+          <div
+            onClick={toggleSearchModal}
+            className={`flex justify-center pt-16 w-full h-full fixed top-0 left-0 bg-gray-900 bg-opacity-80`}
+          ></div>
+          <Search
+            className={"h-[80px] mx-auto !right-0 !left-0 !z-50 absolute"}
+          />
+        </>
       )}
     </>
   );
